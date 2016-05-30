fca558063d0c5df5c982caa0b62fb54d775bfe58
visualkim <visual@adup.kr>
2016-05-30 20:23:16 +0900

    homeworkCalc입니다.

--- app/src/main/java/com/example/cypri/mycalc/MainActivity.java
+++ app/src/main/java/com/example/cypri/mycalc/MainActivity.java
@@ -2,13 +2,8 @@ package com.example.cypri.mycalc;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,14 +11,12 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 
 
-    private Button btnPlus;
-    private Button btnMinus;
-    private Button btnMulti;
-    private Button btnDivi;
-    private Button btnRest;
+    private Button btnPlus,  btnMinus;
+    private Button btnMulti, btnDivi, btnRest;
     private TextView textResult;
-    private EditText input01;
-    private EditText input02;
+    private EditText input01, input02;
+    String num1, num2;
+    Integer result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +31,59 @@ public class MainActivity extends Activity {
         btnDivi = (Button) findViewById(R.id.btnDivi);
         btnRest = (Button) findViewById(R.id.btnDivi);
         textResult = (TextView) findViewById(R.id.textResult);
+
+
+
+        btnPlus.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                num1 = input01.getText().toString();
+                num2 = input02.getText().toString();
+                result = Integer.parseInt(num1) + Integer.parseInt(num2);
+                textResult.setText( result.toString() );
+                return false;
+            }
+        });
+        btnMinus.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                num1 = input01.getText().toString();
+                num2 = input02.getText().toString();
+                result = Integer.parseInt(num1) - Integer.parseInt(num2);
+                textResult.setText( result.toString() );
+                return false;
+            }
+        });
+        btnMulti.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                num1 = input01.getText().toString();
+                num2 = input02.getText().toString();
+                result = Integer.parseInt(num1) * Integer.parseInt(num2);
+                textResult.setText( result.toString() );
+                return false;
+            }
+        });
+        btnDivi.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                num1 = input01.getText().toString();
+                num2 = input02.getText().toString();
+                result = Integer.parseInt( num1 ) / Integer.parseInt( num2 );
+                textResult.setText( result.toString() );
+                return false;
+            }
+        });
+        btnRest.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                num1 = input01.getText().toString();
+                num2 = input02.getText().toString();
+                result = Integer.parseInt( num1 ) % Integer.parseInt( num2 );
+                textResult.setText( result.toString() );
+                return false;
+            }
+        });
 
 
 
